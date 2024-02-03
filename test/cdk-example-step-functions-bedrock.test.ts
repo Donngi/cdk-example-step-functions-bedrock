a40e96a399f340d4d4993070acb395ca9bc91e86
Donngi <22317839+Donngi@users.noreply.github.com>
2024-02-03 14:55:43 +0900

    Add snapshot test

--- test/cdk-example-step-functions-bedrock.test.ts
+++ test/cdk-example-step-functions-bedrock.test.ts
@@ -1,17 +1,14 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkExampleStepFunctionsBedrock from '../lib/cdk-example-step-functions-bedrock-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as CdkExampleStepFunctionsBedrock from "../lib/cdk-example-step-functions-bedrock-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-example-step-functions-bedrock-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkExampleStepFunctionsBedrock.CdkExampleStepFunctionsBedrockStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SnapShot", () => {
+  const app = new cdk.App();
+  const stack =
+    new CdkExampleStepFunctionsBedrock.CdkExampleStepFunctionsBedrockStack(
+      app,
+      "MyTestStack"
+    );
+  const template = Template.fromStack(stack);
+  expect(template.toJSON()).toMatchSnapshot();
 });
